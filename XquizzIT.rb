a5b056e163d08551a639b045a655e936271aa720
Ric-Lavers <30609773+Ric-Lavers@users.noreply.github.com>
2017-08-13 20:14:05 +1000

    better working version

--- XquizzIT.rb
+++ XquizzIT.rb
@@ -1,18 +1,60 @@
 
+
 # XquizzIT is a app that creates user profiles
 # Lets them build their own Quiz
 # lets them complete Quizs
 # records their scores for future logins
 #
+def load_profile(name, password)
+  lines = []
+  File.read("Users/UserProfiles.txt").each_line {|line|
+    lines << line.chomp
+  }
+  for i in 0...lines.length
+    if lines[i] == name
+      if lines[i+1] == password
+        return lines[i], lines[i+1], lines[i+2]
+        break
+      end
+    end
+  end
+  false
+end
+
+def save_profile(name, password)
+  lines = []
+  File.read("Users/UserProfiles.txt").each_line {|line|
+    lines << line.chomp
+  }
+  for i in 0...lines.length
+    if lines[i] == name
+      if lines[i+1] == password
+        lines[i+2] = $currentUser.total_points
+        break
+      end
+    end
+  end
+  File.open("Users/UserProfiles.txt", "w+") do |f|
+    lines.each { |element| f.puts(element) }
+  end
+end
+
+
+require "colorize"
+require "date"
+puts Time.now.strftime("%d/%m/%Y %H:%M:%S %Z")
+
 class UserProfile
 require 'fileutils'
 
-  def initialize(name, password)
+  def initialize(name, password, score=0)
     @name     = name
     @password = password
+    @score    = score
 
   end
   attr_accessor :name, :password
+
   def signIn(name,password)
       if ((@name== name) && (@password == password))
         true
@@ -20,19 +62,18 @@ require 'fileutils'
         false
       end
    end
-  def signUp(name, password)
-    #this create a user profile to a text file.
-    puts "Enter new user name:"
-    name = gets.chomp.strip
-    puts "Enter your password:"
-    password = gets.chomp.strip.to_i
-    File.open("user_login.txt","a"){ |f|
-      f << name }
-  end
 
-  def add_score(score)
-    #this updates the quiz score to the user profile which is text file
-  end
+
+  # def signUp(name, password)
+  #   #this create a user profile to a text file.
+  #   puts "Enter new user name:"
+  #   name = gets.chomp.strip
+  #   puts "Enter your password:"
+  #   password = gets.chomp.strip.to_i
+  #   File.open("user_login.txt","a"){ |f|
+  #     f << name }
+  # end
+
 end
 ###############
 def load_quiz (topic)
@@ -92,14 +133,14 @@ class Quiz
 
 
       puts "Question #{count + 1}".yellow
-      puts "#{question}".yellow
+      puts "#{question}".green
       answer = gets.chomp.capitalize
       if answer == ans
-        puts "Got it!"
+        puts "Got it!".blue
         points = 1
       elsif answer.count(ans)/ans.length.to_f >0.6
-        puts "oh thats so close, but spelling is important"
-        puts "but I'll give you almost full points.\n it's spelt #{ans}"
+        puts "oh thats so close, but spelling is important".blue
+        puts "but I'll give you almost full points.\n it's spelt #{ans}".blue
         points = 0.5
       else
         puts "thats incorrect".red
@@ -115,49 +156,6 @@ class Quiz
   end
 end
 #####
-# system("clear")
-# puts "Welcome to QuizeIT game"
-# puts "Do you have a profile? [y,n]"
-# yorn = gets.chomp.downcase
-# if yorn == "n"
-#   puts "please enter your name"
-#   name = gets.chomp.capitalize
-#   puts "please enter password in numbers"
-#   pw = gets.chomp.to_i
-#   user = UserProfile.new(name,pw)
-# elsif yorn =="y"
-#   puts "..."
-# else
-#   puts "error"
-# end
-# count = 0
-# while count < 3
-#   system("clear")
-#   puts "Please enter user name"
-#   currenName = gets.chomp.strip
-#   puts "Login : #{currenName}"
-#   puts "Enter password:"
-#   currentPassword = gets.chomp.strip.to_i
-#
-#   if (user.signIn(currenName,currentPassword) == true)
-#     t = Time.now
-#     score = 300
-#     File.open("user_info.txt","a"){}
-#     puts "Hey #{user.name}, test score had: #{score} on #{t.strftime("%d/%m/%Y %H:%M:%S")}"
-#
-#     break
-#   else
-#     puts "You enter wrong user login and password"
-#     count += 1
-#     puts "count is #{count}"
-#     if (count == 3)
-#        puts "Your account had been locked after 3 times."
-#     end
-#   end
-#
-# end
-#########
-
 
 #BUILD THE QUIZ
 def make_quiz
@@ -178,6 +176,7 @@ def load_screen
   for i in 0...ls.count
     ls[i] = ls[i].gsub("/", "").gsub("quiz", "")
   end
+  ls.pop
   quizs = []
   ls.each{|topic|
     item = load_quiz(topic)
@@ -187,33 +186,108 @@ def load_screen
 
 end
 
+class Points
+  #initialize the class from user profile
+  def initialize(points, total_questions)
+    @total_points  = points
+    @total_quest   = total_questions
+  end
+  attr_accessor :total_points, :total_quest
+  #use to add more points to users total points
+  def addPoints(score)
+    @total_points += score
+  end
+  def addQuest(num)
+    @total_quest += num
+  end
+  # create a percentage
+  def percent(score, questions)
+    questions/score*100
+  end
+  #total percentage
+  def totalPercent()
+    @total_points/@total_quest.to_f*100
+  end
+  def save_score
+  end
+end
+
+def login
+  puts "Welcome to XquizzIT \n"
+  puts "Do you have a profile? [y/n]"
+  yn = gets.chomp.downcase
+  case yn
+  when "y"
+    puts "please enter your name".green
+    name = gets.chomp.capitalize
+    puts "please enter password in numbers".green
+    pw = gets.chomp.to_s
+    if load_profile(name, pw) == false
+      puts "No profile found"
+      login
+    end
+    previousUser = load_profile(name, pw)
+    $user = UserProfile.new(name,pw)
+    $currentUser = Points.new(previousUser[2].to_i,0)
+  when "n"
+    puts "please enter your name".green
+    name = gets.chomp.capitalize
+    puts "please enter password in numbers".green
+    pw = gets.chomp.to_s
+    $user = UserProfile.new(name,pw)
+    File.open("Users/UserProfiles.txt","a"){ |f|
+      f << "\n#{name}"
+      f << "\n#{pw}"
+      f << "\n0"}
+    $currentUser = Points.new(0,0)
+  else
+    login
+  end
+  puts "your total points are #{$currentUser.total_points}"
+end
+
+
 def menu
-  puts "MENU"
+  puts "________________________"
+  puts "MENU".yellow
   puts ""
-  puts "[1] Complete a quiz \n[2] Make Quiz \n[3] Quit"
+  puts "[1] Complete a quiz \n[2] Make Quiz \n[3] Quit".green
   choice = gets.chomp.to_i
   case choice
   when 1
-    chosenQuiz = load_screen
-    puts "Choose a quiz from below"
+    $chosenQuiz = load_screen
+
+    puts "Choose a quiz from below".yellow
     puts ""
     count = 1
-    chosenQuiz.each{|name|
-      puts "[#{count}] #{name.topic}"
+    $chosenQuiz.each{|name|
+      puts "[#{count}] #{name.topic}".green
       count +=1}
     choice = (gets.chomp.to_i )- 1
-    if choice <0 || choice > chosenQuiz.count
+    if choice <0 || choice > $chosenQuiz.count
       puts "please make a valid choice"
       load_screen
     end
-    puts "how many questions would you like max is #{chosenQuiz[choice].num}"
+    puts "how many questions would you like max is #{$chosenQuiz[choice].num}".yellow
     num_of_q = gets.chomp.to_i
-    chosenQuiz[choice].do_quiz(num_of_q)
+    points = $chosenQuiz[choice].do_quiz(num_of_q)
+    $currentUser.addPoints(points)
+    $currentUser.addQuest(num_of_q)
+    puts "you scored points are #{points} out of #{num_of_q}".yellow
+    puts "Thats #{$currentUser.percent(points, num_of_q)}%".yellow
+    menu
   when 2
     make_quiz
     menu
   when 3
-    puts "bye"
+    puts "your total points are #{$currentUser.total_points}"
+    sleep(2)
+    puts "bye "
+    #find users score line in userProfiles.txt
+    #update text file to refelct final score
+    print $user.name
+    puts ""
+    save_profile($user.name, $user.password)
     #ARE USER SCORES SAVED???
     abort
   else
@@ -223,17 +297,10 @@ def menu
   end
 end
 
-puts load_quiz("animals")
-
-#Quiz.new("animals", n, q, a)
+login
+for i in 0..3
+  print "*"
+  sleep(1)
+end
+system("clear")
 menu
-# puts "please choose your quiz topic \n #{topic}"
-#
-# #topic.load_quiz
-# # puts topic.q
-# # puts topic.a
-# #topic.do_quiz(4)
-#
-# puts "#{topic.do_quiz(4)} out of #{4}"
-
-#write score to user profile
